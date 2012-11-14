05bc83893bcf803cfc831d4f1c10257a7840d566
russell <email@emsil.com>
2012-11-14 04:18:53 +0000

    initial

---
+++ secret.h
@@ -0,0 +1,9 @@
+#ifndef __HELLO_H
+#define __HELLO_H
+
+/** The Hello, World! message. */
+#define HELLO_MESSAGE "Hello, World New World!\n"
+
+#define SECRET_SIZE 8192
+
+#endif /* __HELLO_H */
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+
+  printf("hello world");
+  return 1;
+}
